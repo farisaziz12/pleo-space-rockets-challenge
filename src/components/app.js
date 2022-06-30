8f22d02aec9de9afcad094784ffebf872c6a41eb
Faris Aziz <farisaziz12@gmail.com>
2022-06-30 13:20:42 +0200

    feat(app): add fav provider and drawer

--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, Text, Button } from "@chakra-ui/core";
 
 import Launches from "./launches";
 import Launch from "./launch";
 import Home from "./home";
 import LaunchPads from "./launch-pads";
 import LaunchPad from "./launch-pad";
+import FavoritesDrawer from "./favorites-drawer";
+import { FavoritesProvider } from "../contexts";
 
 export default function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/launches" element={<Launches />} />
-        <Route path="/launches/:launchId" element={<Launch />} />
-        <Route path="/launch-pads" element={<LaunchPads />} />
-        <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
-    </div>
+    <>
+      <NavBar openDrawer={() => setIsDrawerOpen(true)} />
+      <FavoritesProvider>
+        <FavoritesDrawer onClose={() => setIsDrawerOpen(false)} isOpen={isDrawerOpen} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/launches" element={<Launches />} />
+          <Route path="/launches/:launchId" element={<Launch />} />
+          <Route path="/launch-pads" element={<LaunchPads />} />
+          <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
+        </Routes>
+      </FavoritesProvider>
+    </>
   );
 }
 
-function NavBar() {
+function NavBar({ openDrawer }) {
   return (
     <Flex
       as="nav"
@@ -34,14 +41,12 @@ function NavBar() {
       bg="gray.800"
       color="white"
     >
-      <Text
-        fontFamily="mono"
-        letterSpacing="2px"
-        fontWeight="bold"
-        fontSize="lg"
-      >
+      <Text fontFamily="mono" letterSpacing="2px" fontWeight="bold" fontSize="lg">
         ¡SPACE·R0CKETS!
       </Text>
+      <Button onClick={openDrawer} color="black" fontWeight="bold" fontSize="sm">
+        Favorites
+      </Button>
     </Flex>
   );
 }
